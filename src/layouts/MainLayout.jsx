bcca388dc4685ecbc5fd915ef90a1d79100d80f1
Samuel Adzoho <edinamsam4@gmail.com>
2026-02-16 11:39:51 +0000

    feat: add floating animated background blur shapes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,6 +5,11 @@ import Footer from "../components/layout/Footer";
 function MainLayout() {
   return (
     <div className="app">
+      <div className="bg-blobs">
+        <div className="blob blob-1"></div>
+        <div className="blob blob-2"></div>
+        <div className="blob blob-3"></div>
+      </div>
       <Navbar />
       <main className="main">
         <div className="container">
