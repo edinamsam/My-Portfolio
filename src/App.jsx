bf1fe563bbba9d4a4a3720e830b9f1fe35d898ab
Samuel Adzoho <edinamsam4@gmail.com>
2026-02-17 13:28:35 +0000

    feat: add features to projects page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Route>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,60 @@
+import { span } from "framer-motion/client";
+
+const projects = [
+  {
+    id: 1,
+    title: "Weather Dashboard",
+    description:
+      "A responsive weather app using Open-Meteo API with dynamic unit toggle and forecast rendering.",
+    stack: ["React", "API", "CSS Grid"],
+    link: "https://weather-app-five-rho-73.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Comment Section App",
+    description:
+      "Interactive comment system with replies, editing, deletion, and localStorage persistence.",
+    stack: ["JavaScript", "LocalStorage", "Responsive UI"],
+    link: "https://comment-section-app-kappa.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Digital Bank Landing Page",
+    description:
+      "Modern responsive landing page with animated hero and mobile navigation.",
+    stack: ["HTML", "CSS", "Flexbox"],
+    link: "https://bank-landing-page-wheat.vercel.app/",
+  },
+];
+
 function Projects() {
   return (
-    <>
-      <h1>Samuel's projects.</h1>
-    </>
+    <section className="projects">
+      <div className="projects-container">
+        <h1 className="projects-title">Selected Work</h1>
+
+        <div className="projects-grid">
+          {projects.map((project) => {
+            return (
+              <div className="project-card" key={project.id}>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+
+                <div className="project-stack">
+                  {project.stack.map((tech, index) => (
+                    <span key={index}>{tech}</span>
+                  ))}
+                </div>
+
+                <a href={project.link} className="project-link">
+                  View project →
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
 
